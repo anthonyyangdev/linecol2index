acd3c61913ee72844fe9d91ec63a5575fea419c7
Anthony Yang <ay339@cornell.edu>
2020-09-03 06:55:10 -0400

    Update docs

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 /**
  * Maps the given line number and col number to the corresponding index of the given string.
  *
- * Returns -1 if an index is not possible for the given string, such as the given column number
- * exceeds the actual number of columns in the string.
+ * Returns -1 if the index cannot be found for the given string, such as if the given column number
+ * exceeds the actual number of columns used in the string.
  * @param line
  * @param col
  * @param text
